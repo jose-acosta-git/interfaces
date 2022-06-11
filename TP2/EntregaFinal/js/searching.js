60f69f548e1ff0657511742000a5104537fc2a02
jose-acosta-git <jose.acosta.git@gmail.com>
2022-06-10 23:39:13 -0300

    hardcodea todos los juegos

--- TP2/EntregaFinal/js/searching.js
+++ TP2/EntregaFinal/js/searching.js
@@ -8,7 +8,99 @@ const games = [
     {
         "name": "Sonic",
         "image": "images/sonic.jpg"
-    }
+    },
+    {
+        "name": "Damas",
+        "image": "images/damas.jpg"
+    },
+    {
+        "name": "Dominó",
+        "image": "images/domino.jpg"
+    },
+    {
+        "name": "Pacman",
+        "image": "images/pacman.jpg"
+    },
+    {
+        "name": "Undertale",
+        "image": "images/undertale.jpg"
+    },
+    {
+        "name": "Final Fantasy III",
+        "image": "images/finalfantasy.png"
+    },
+    {
+        "name": "Bomberman",
+        "image": "images/Bomberman.jpg"
+    },
+    {
+        "name": "Kirby",
+        "image": "images/kirby.jpg"
+    },
+    {
+        "name": "Pro Evolution Soccer 2006",
+        "image": "images/pes6.jpg"
+    },
+    {
+        "name": "The Leyend of Zelda",
+        "image": "images/zelda.jpg"
+    },
+    {
+        "name": "Poker",
+        "image": "images/poker.jpg"
+    },
+    {
+        "name": "Crash Bandicoot",
+        "image": "images/crash bandicoot.jpg"
+    },
+    {
+        "name": "Pokémon Esmeralda",
+        "image": "images/pokemon esmeralda.jpg"
+    },
+    {
+        "name": "Tetris",
+        "image": "images/tetris.jpg"
+    },
+    {
+        "name": "Truco",
+        "image": "images/truco.jpg"
+    },
+    {
+        "name": "Megaman II",
+        "image": "images/megaman.png"
+    },
+    {
+        "name": "Super Smash Bros",
+        "image": "images/supersmash.jpg"
+    },
+    {
+        "name": "Solitario",
+        "image": "images/solitario.jpg"
+    },
+    {
+        "name": "Ajedrez",
+        "image": "images/ajedrez.png"
+    },
+    {
+        "name": "Monopoly",
+        "image": "images/monopoly.jpg"
+    },
+    {
+        "name": "Mahjong",
+        "image": "images/mahjong.png"
+    },
+    {
+        "name": "Uno",
+        "image": "images/uno.jpg"
+    },
+    {
+        "name": "Parchis",
+        "image": "images/parchis.png"
+    },
+    {
+        "name": "Bingo",
+        "image": "images/Bingo.jpg"
+    },
 ];
 
 document.addEventListener("DOMContentLoaded", init);
