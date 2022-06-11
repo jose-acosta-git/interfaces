4bd01a1ffe43a648d8e5d88a1e26d57eb661f109
jose-acosta-git <jose.acosta.git@gmail.com>
2022-06-10 23:17:44 -0300

    agrega busqueda en desktop

--- TP2/EntregaFinal/js/search.js
+++ TP2/EntregaFinal/js/search.js
@@ -3,13 +3,15 @@
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    let formSearch = document.querySelector('#search');
-    let inputSearcch = document.querySelector('#input-search');
+    let formSearch = document.querySelectorAll('.search');
 
-    formSearch.addEventListener("submit", function(e) {
-        e.preventDefault();
-        search(inputSearcch.value);
-    });
+    formSearch.forEach(form => {
+        form.addEventListener("submit", function(e) {
+            console.log("entro");
+            e.preventDefault();
+            search(form.firstElementChild.value);
+        });
+    })
 
     function search(searching) {
         window.location.href = "searching.html?" + searching;
